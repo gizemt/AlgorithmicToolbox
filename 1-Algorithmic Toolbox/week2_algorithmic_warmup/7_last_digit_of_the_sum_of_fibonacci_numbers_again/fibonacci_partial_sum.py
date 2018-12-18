3882397b195539d64433c022d3c8a62e7ee44670
gizemt <gizemtabak7@hotmail.com>
2018-12-18 11:06:48 -0600

    day 2

--- 1-Algorithmic Toolbox/week2_algorithmic_warmup/7_last_digit_of_the_sum_of_fibonacci_numbers_again/fibonacci_partial_sum.py
+++ 1-Algorithmic Toolbox/week2_algorithmic_warmup/7_last_digit_of_the_sum_of_fibonacci_numbers_again/fibonacci_partial_sum.py
@@ -15,8 +15,31 @@ def fibonacci_partial_sum_naive(from_, to):
 
     return sum % 10
 
+# Gizem
+def get_fibonacci_mod(m):
+    if (m == 0):
+        return m, 0
+    elif m == 1:
+        return [0, 1], [0, 1] 
+    else:
+        fb = [0, 1]
+        cumsum = [0, 1]
+        while len(fb)==2 or not (fb[-1] == 1 and fb[-2] == 0):
+            fb.append((fb[-1] + fb[-2])%m)
+            cumsum.append((cumsum[-1]+fb[-1])%m)
+        return fb[:-2], cumsum[:-2]
+
+def fibonacci_sum_last_digit(n):
+    fb_mod_array, cumsum_array = get_fibonacci_mod(10)
+    return ((cumsum_array[-1])*int(n/(len(fb_mod_array))) + cumsum_array[n%(len(fb_mod_array))])%10
+
+def fibonacci_partial_sum(from_, to):
+    return (fibonacci_sum_last_digit(to)-fibonacci_sum_last_digit(from_-1))%10
+
 
 if __name__ == '__main__':
     input = sys.stdin.read();
     from_, to = map(int, input.split())
-    print(fibonacci_partial_sum_naive(from_, to))
+    # print(fibonacci_partial_sum_naive(from_, to))
+    print(fibonacci_partial_sum(from_, to))
+
--- 1-Algorithmic Toolbox/week2_algorithmic_warmup/8_last_digit_of_the_sum_of_squares_of_fibonacci_numbers/fibonacci_sum_squares.py
+++ 1-Algorithmic Toolbox/week2_algorithmic_warmup/8_last_digit_of_the_sum_of_squares_of_fibonacci_numbers/fibonacci_sum_squares.py
@@ -15,6 +15,26 @@ def fibonacci_sum_squares_naive(n):
 
     return sum % 10
 
+# Gizem
+def get_fibonacci_mod(m):
+    if (m == 0):
+        return m
+    elif m == 1:
+        return [0, 1] 
+    else:
+        fb = [0, 1]
+        while len(fb)==2 or not (fb[-1] == 1 and fb[-2] == 0):
+            fb.append((fb[-1] + fb[-2])%m)
+        return fb[:-2]
+
+def get_fibonacci_huge(n, m):
+    fb_mod_array = get_fibonacci_mod(m)
+    return fb_mod_array[n%(len(fb_mod_array))]
+
+def fibonacci_sum_squares(n):
+    return (get_fibonacci_huge(n, 10)*get_fibonacci_huge(n+1, 10))%10
+
 if __name__ == '__main__':
     n = int(stdin.read())
-    print(fibonacci_sum_squares_naive(n))
+    # print(fibonacci_sum_squares_naive(n))
+    print(fibonacci_sum_squares(n))
