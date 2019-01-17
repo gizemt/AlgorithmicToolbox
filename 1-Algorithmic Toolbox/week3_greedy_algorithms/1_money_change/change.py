7f64918fe31e31de7da3e265c61c20b45a2a4746
gizemt <gizemtabak7@hotmail.com>
2019-01-17 16:10:45 -0600

    Day 3

--- 1-Algorithmic Toolbox/week3_greedy_algorithms/1_money_change/change.py
+++ 1-Algorithmic Toolbox/week3_greedy_algorithms/1_money_change/change.py
@@ -3,7 +3,10 @@ import sys
 
 def get_change(m):
     #write your code here
-    return m
+    n10 = int(m/10)
+    n5 = int((m - n10*10)/5)
+    n1 = (m - n10*10 - n5*5)
+    return n10 + n5 + n1
 
 if __name__ == '__main__':
     m = int(sys.stdin.read())
