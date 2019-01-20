c12b2b9d6c981d4335505af70d107bf4a078b743
gizemt <gizemtabak7@hotmail.com>
2019-01-20 17:11:06 -0600

    Week 3

--- 1-Algorithmic Toolbox/week3_greedy_algorithms/6_maximum_salary/largest_number.py
+++ 1-Algorithmic Toolbox/week3_greedy_algorithms/6_maximum_salary/largest_number.py
@@ -5,13 +5,22 @@ import sys
 def largest_number(a):
     #write your code here
     res = ""
-    for x in a:
-        res += x
-    return res
+    while len(a) > 0:
+        x_max = str(0)
+        for x in a:
+            if int(x[0]) > int(x_max[0]):
+                x_max = x
+            elif int(x[0]) == int(x_max[0]):
+                sm = x + x_max
+                ms = x_max + x
+                if int(sm[:max(len(x), len(x_max))]) > int(ms[:max(len(x), len(x_max))]):
+                    x_max = x
+        res += x_max
+        a.remove(x_max)
+    return int(res)
 
 if __name__ == '__main__':
     input = sys.stdin.read()
     data = input.split()
     a = data[1:]
     print(largest_number(a))
-    
