816e77ac43b6653e8a1a05c4279e60bf6526c547
gizemt <gizemtabak7@hotmail.com>
2019-01-26 22:48:14 -0600

    Week 4

--- 1-Algorithmic Toolbox/week4_divide_and_conquer/5_organizing_a_lottery/points_and_segments.py
+++ 1-Algorithmic Toolbox/week4_divide_and_conquer/5_organizing_a_lottery/points_and_segments.py
@@ -1,9 +1,10 @@
 # Uses python3
-import sys
+import sys 
 
 def fast_count_segments(starts, ends, points):
     cnt = [0] * len(points)
     #write your code here
+
     return cnt
 
 def naive_count_segments(starts, ends, points):
